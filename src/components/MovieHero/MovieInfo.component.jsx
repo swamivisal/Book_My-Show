5f1d78ae69e59c6774a5db50bb77fef6fdfc8ee6
swamivisal <swaminathank2001@gmail.com>
2021-07-27 23:35:06 +0530

    Movie page modified

--- src/components/MovieHero/MovieInfo.component.jsx
+++ src/components/MovieHero/MovieInfo.component.jsx
@@ -1,14 +1,26 @@
-import React,{useContext} from "react";
+import React,{useContext,useState} from "react";
 import { MovieContext } from "../../context/movie.context";
-
+import PaymentModal from "../PaymentModel/Payment.component";
 
 const MovieInfo = () => {
+  const [isOpen,setIsOpen]=useState(false);
+  const [price,setPrice]=useState(0);
   const {movie}=useContext(MovieContext);
   
   //optional chaining
   const genres=movie.genres?.map(({name})=>name).join(", ");
+
+  const rentMovies=()=>{
+    setIsOpen(true);
+    setPrice(149);
+  }
+  const buyMovies=()=>{
+    setIsOpen(true);
+    setPrice(649);
+  }
   return (
     <>
+    <PaymentModal setIsOpen={setIsOpen} isOpen={isOpen} price={price}/>
       <div className="flex flex-col gap-3 lg:gap-8">
         <div className="flex items-center gap-3 md:px-4">
           <div className="w-40 h-8">
@@ -28,14 +40,14 @@ const MovieInfo = () => {
         <div className="flex flex-col-reverse gap-3 lg:gap-5 lg:flex-col">
           <div className="text-white font-light flex flex-col gap-2 md:px-4">
             <h4>4k &bull; {movie.original_language} </h4>
-            <h4>{(movie.runtime/60).toFixed(2)} h &bull; {genres} &bull; 18+</h4>
+            <h4>{(movie.runtime/60).toFixed(2)} h &bull; {genres} </h4>
           </div>
           <div className="flex items-center gap-3 md:px-4 md:w-screen lg:w-full">
-            <button className="bg-red-600 w-full py-3 text-white font-semibold rounded-lg">
+            <button onClick={rentMovies} className="bg-red-600 w-full py-3 text-white font-semibold rounded-lg">
               Rent ₹149
             </button>
-            <button className="bg-red-600 w-full py-3 text-white font-semibold rounded-lg">
-              Buy ₹149
+            <button onClick={buyMovies} className="bg-red-600 w-full py-3 text-white font-semibold rounded-lg">
+              Buy ₹649
             </button>
           </div>
         </div>
